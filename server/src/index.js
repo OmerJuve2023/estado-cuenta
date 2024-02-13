3fb7cb7b14494c697c68268e38270e21eb3a1d04
Omar Rafael Toribio <106570431+OmerJuve2023@users.noreply.github.com>
2024-02-12 21:48:29 -0500

    add routes to order.routes.js successful

--- server/src/index.js
+++ server/src/index.js
@@ -5,6 +5,7 @@ import customerRoutes from "./routes/customer.routes.js";
 import indexRoutes from "./routes/index.routes.js";
 import productRoutes from "./routes/product.routes.js";
 import orderRoutes from "./routes/order.routes.js";
+import orderDetailRoutes from "./routes/orderDetail.routes.js";
 
 const app = express();
 
@@ -16,6 +17,8 @@ app.use("/api/customer", customerRoutes);
 app.use("/api/index", indexRoutes);
 app.use("/api/product", productRoutes);
 app.use("/api/order", orderRoutes);
+app.use("/api/orderDetail", orderDetailRoutes);
+
 
 app.listen(PORT);
 console.log(`Server on port http://localhost:${PORT}`);
